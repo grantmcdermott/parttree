a7646b75853a4fc63b7e5381a2d75f988dce7a1b
Grant McDermott <grant.mcdermott@gmail.com>
2021-08-19 16:46:50 -0700

    Add tests

---
+++ inst/tinytest/test_continuous_depvar.R
@@ -0,0 +1,20 @@
+## Comparison data
+d =
+  data.frame(
+    node = c(5, 7, 8, 9, 13, 24, 25),
+    Sepal.Length = c(5.640000, 7.577778, 4.735000, 5.169697, 6.604000, 6.054545, 6.530000),
+    path = c("Petal.Length < 4.25 --> Petal.Length >= 3.4" ,
+             "Petal.Length >= 4.25 --> Petal.Length >= 6.05",
+             "Petal.Length < 4.25 --> Petal.Length < 3.4 --> Sepal.Width < 3.25",
+             "Petal.Length < 4.25 --> Petal.Length < 3.4 --> Sepal.Width >= 3.25",
+             "Petal.Length >= 4.25 --> Petal.Length < 6.05 --> Petal.Length >= 5.15",
+             "Petal.Length >= 4.25 --> Petal.Length < 6.05 --> Petal.Length < 5.15 --> Sepal.Width < 3.05",
+             "Petal.Length >= 4.25 --> Petal.Length < 6.05 --> Petal.Length < 5.15 --> Sepal.Width >= 3.05"),
+    xmin = c(3.40, 6.05, -Inf, -Inf, 5.15, 4.25, 4.25),
+    xmax = c(4.25, Inf, 3.40, 3.40, 6.05, 5.15, 5.15),
+    ymin = c(-Inf, -Inf, -Inf, 3.25, -Inf, -Inf, 3.05),
+    ymax = c(Inf, Inf, 3.25, Inf, Inf, 3.05, Inf)
+  )
+
+rp = rpart::rpart(Sepal.Length ~ Petal.Length + Sepal.Width, data = iris)
+expect_equal(d, parttree(rp), tolerance = 1e-7)
--- inst/tinytest/test_parttree.R
+++ inst/tinytest/test_parttree.R
@@ -1,4 +1,30 @@
+## Comparison data
+d =
+  data.frame(
+    node = c(2, 6, 7),
+    Species = factor(c("setosa", "versicolor", "virginica")),
+    path = c("Petal.Length < 2.45",
+             "Petal.Length >= 2.45 --> Petal.Width < 1.75",
+             "Petal.Length >= 2.45 --> Petal.Width >= 1.75"),
+    xmin = c(-Inf, 2.45, 2.45),
+    xmax = c(2.45, Inf, Inf),
+    ymin = c(-Inf, -Inf, 1.75),
+    ymax = c(Inf, 1.75, Inf)
+  )
 
-# Placeholder with simple test
-expect_equal(1 + 1, 2)
+## Formula
+fml = formula(Species ~ Petal.Length + Petal.Width)
+
+## rpart
+rp = rpart::rpart(fml, data = iris)
+expect_equal(d, parttree(rp))
+
+## partykit
+if (require(partykit)) {
+  rp2 = as.party(rp)
+  pt_rp2 = parttree(rp2)
+  row.names(pt_rp2) = NULL
+  expect_equal(d[, setdiff(names(d), 'node')],
+               pt_rp2[, setdiff(names(pt_rp2), 'node')])
+}
 
