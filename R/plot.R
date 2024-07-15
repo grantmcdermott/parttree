ddde2b44064715e2df5fedf8c9f338d2ff12e5ac
Grant McDermott <grant.mcdermott@gmail.com>
2024-07-15 12:30:10 -0700

    use tinyplot empty arg
    - also warn user if orginal data cannot be retrieved

--- R/plot.R
+++ R/plot.R
@@ -46,31 +46,79 @@ plot.parttree =
         xrange = attr(object, "parttree")[["xrange"]]
         yrange = attr(object, "parttree")[["yrange"]]
         response = attr(object, "parttree")[["response"]]
+        raw_data = attr(object, "parttree")[["raw_data"]]
         orig_call = attr(object, "parttree")[["call"]]
-        na.action = attr(object, "parttree")[["na.action"]]
+        orig_na_idx = attr(object, "parttree")[["na.action"]]
 
-        raw_data = eval(orig_call$data)[, c(response, xvar, yvar)]
+        if (isTRUE(raw)) {
+            if (!is.null(raw_data)) {
+                raw_data = eval(raw_data)
+            } else {
+                raw_data = eval(orig_call$data)[, c(response, xvar, yvar)]
+                if (!is.null(orig_na_idx)) raw_data = raw_data[-orig_na_idx, , drop = FALSE]
+            }
+            if (is.null(raw_data)){
+                warning(
+                    "\nCould not find original data. Ignoring.",
+                    "\n(Did you delete the original model object?)\n"
+                )
+                raw = FALSE
+            }
+
+        }
+
         plot_fml = reformulate(paste(xvar, "|", response), response = yvar)
 
         object$response = object[[response]]
 
-        tinyplot(
-            plot_fml,
-            data = raw_data,
-            type = "n",
-            col = border,
-            legend = list(pt.cex = 3.5, pch = 22, y.intersp = 1.25, x.intersp = 1.25),
-            fill = fill_alpha,
-            ...
+        # tinyplot(
+        #     plot_fml,
+        #     data = raw_data,
+        #     type = "n",
+        #     col = border,
+        #     legend = list(pt.cex = 3.5, pch = 22, y.intersp = 1.25, x.intersp = 1.25),
+        #     fill = fill_alpha,
+        #     ...
+        # )
+
+        xmin_idxr = object$xmin == -Inf
+        xmax_idxr = object$xmax == Inf
+        ymin_idxr = object$ymin == -Inf
+        ymax_idxr = object$ymax == Inf
+
+        object$response = object[[response]]
+
+        object$xmin[xmin_idxr] = xrange[1]
+        object$xmax[xmax_idxr] = xrange[2]
+        object$ymin[ymin_idxr] = yrange[1]
+        object$ymax[ymax_idxr] = yrange[2]
+
+        with(
+            object,
+            tinyplot(
+                x = xmin,
+                xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax,
+                by = response,
+                type = "rect",
+                col = border,
+                fill = fill_alpha,
+                empty = TRUE,
+                ...
+            )
         )
 
         corners = par("usr")
 
-        object$xmin[object$xmin == -Inf] = corners[1]
-        object$xmax[object$xmax == Inf] = corners[2]
-        object$ymin[object$ymin == -Inf] = corners[3]
-        object$ymax[object$ymax == Inf] = corners[4]
-        object$response = object[[response]]
+        # object$xmin[object$xmin == -Inf] = corners[1]
+        # object$xmax[object$xmax == Inf] = corners[2]
+        # object$ymin[object$ymin == -Inf] = corners[3]
+        # object$ymax[object$ymax == Inf] = corners[4]
+        object$xmin[xmin_idxr] = corners[1]
+        object$xmax[xmax_idxr] = corners[2]
+        object$ymin[ymin_idxr] = corners[3]
+        object$ymax[ymax_idxr] = corners[4]
+
+
 
         with(
             object,
